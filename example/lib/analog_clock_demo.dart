c1c14c67c753cd0f1d40295bd6a216619b2d48c8
conghaonet <hao.cong@app2m.com>
2019-11-28 21:02:49 +0800

    finish demo

--- example/lib/analog_clock_demo.dart
+++ example/lib/analog_clock_demo.dart
@@ -6,16 +6,38 @@ class AnalogClockDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.blue,
-        child: Container(
-          color: Colors.red,
-//          width: 200,
-//          height: 200,
-          child: AnalogClock.dark(
-            hourNumberScale: 0.5,
-//            borderColor: Colors.grey,
-          ),
+      body: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Expanded(child: buildClock1(),),
+                  Expanded(child: buildClock2(),),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Expanded(child: buildClock3(),),
+                  Expanded(child: buildClock4(),),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Expanded(child: buildClock5(),),
+                  Expanded(child: buildClock6(),),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -23,6 +45,90 @@ class AnalogClockDemo extends StatelessWidget {
 
 }
 
-//Widget clock1() {
-//  return Analo
-//}
+Widget buildClock1() {
+  return AnalogClock();
+}
+
+Widget buildClock2() {
+  DateTime dateTime = DateTime.now();
+  dateTime = dateTime.add(Duration(hours: 8));
+  return AnalogClock.dark(
+    dateTime: dateTime,
+    child: Center(
+      child: Padding(
+        padding: EdgeInsets.only(top: 100),
+        child: Text(
+          'GMT + 8',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget buildClock3() {
+  return AnalogClock(
+    hourNumbers: ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII'],
+    dialPlateColor: Colors.yellow,
+    hourHandColor: Colors.red,
+    minuteHandColor: Colors.green,
+    tickColor: Colors.green,
+    numberColor: Colors.blue,
+    centerPointColor: Colors.white,
+    borderWidth: 0,
+    showSecondHand: false,
+    showTicks: false,
+  );
+}
+
+Widget buildClock4() {
+  DateTime dateTime = DateTime.now();
+  dateTime = dateTime.add(Duration(hours: -8));
+  return AnalogClock(
+    child: Center(
+      child: Padding(
+        padding: EdgeInsets.only(bottom: 100),
+        child: Text(
+          'GMT - 8',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    ),
+    dateTime: dateTime,
+    dialPlateColor: Colors.green,
+    hourHandColor: Colors.red,
+    minuteHandColor: Colors.purple,
+    secondHandColor: Colors.blue[200],
+    borderColor: Colors.green[700],
+    tickColor: Colors.white,
+    centerPointColor: Colors.yellow,
+    showNumber: false,
+  );
+}
+
+Widget buildClock5() {
+  return AnalogClock(
+    dialPlateColor: Colors.blue,
+    hourHandColor: Colors.white,
+    centerPointColor: Colors.white,
+    showMinuteHand: false,
+    showSecondHand: false,
+    showBorder: false,
+    showTicks: false,
+    showNumber: false,
+  );
+}
+
+Widget buildClock6() {
+  return AnalogClock(
+    hourNumbers: ['', '', '3', '', '', '6', '', '', '9', '', '', '12'],
+    dialPlateColor: Colors.red,
+    hourHandColor: Colors.white,
+    minuteHandColor: Colors.white,
+    secondHandColor: Colors.white,
+    centerPointColor: Colors.white,
+    borderColor: Colors.red[700],
+    borderWidth: 24,
+    tickColor: Colors.blue[900],
+  );
+}
